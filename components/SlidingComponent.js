08f11c5835da99498a34432f52ef4569fd7a6ec8
Akhil Juluru <akhil.juluru@gmail.com>
2022-02-25 12:55:10 +0530

    mobile styles

--- components/SlidingComponent.js
+++ components/SlidingComponent.js
@@ -1,20 +1,22 @@
 import React from "react"
 import { NextArrow, PrevArrow } from "./Arrows"
 import Small from "./title/Small"
+import { useWindowSize } from "../utils/hooks"
 
 function SlidingComponent({ data }) {
+  const { width } = useWindowSize()
   return (
     <div className="w-100">
-      <h2 className="font-semibold text-2xl mt-4 text-white mx-8">
+      <h2 className="font-semibold text-2xl mt-4 text-white mx-4 md:mx-8">
         {data.title}
       </h2>
       <div className="py-4 space-y-4 scroll-smooth overflow-x-scroll no-scrollbar overflow-y-hidden relative group">
-        <div className="flex pl-8 gap-4">
-          <PrevArrow />
+        <div className="flex pl-4 md:pl-8 gap-4">
+          {width >= 900 && <PrevArrow />}
           {data.info.map((item) => (
             <Small item={item} key={item.title} />
           ))}
-          <NextArrow />
+          {width >= 900 && <NextArrow />}
         </div>
       </div>
     </div>
